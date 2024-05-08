41013e037fad09377cf57b1060fcf750a9a225b5
tabatsky <e.tabatsky@gmail.com>
2024-05-09 00:27:38 +0300

    Initial Commit

---
+++ SoxTest_iOS/SoxTest_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoxTest_iOSApp.swift
+//  SoxTest_iOS
+//
+//  Created by User on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SoxTest_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
